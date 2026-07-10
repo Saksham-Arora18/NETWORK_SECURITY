1f4f41fd1fe6d58ec4af7f5e5779358d2a0b51b6
Saksham-Arora18 <saksham0666@gmail.com>
2026-07-10 14:59:56 +0530

    TRACKING WITH DAGSHUB REPOSITORY

--- network_security/components/model_trainer.py
+++ network_security/components/model_trainer.py
@@ -25,7 +25,10 @@ from sklearn.ensemble import (
 )
 import mlflow
 from urllib.parse import urlparse
+import dagshub
 
+import dagshub
+dagshub.init(repo_owner='saksham0666', repo_name='NETWORK_SECURITY', mlflow=True)
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
