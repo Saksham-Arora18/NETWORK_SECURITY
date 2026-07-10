ce4d04767c96ce75e5f4cc84118f831bf43125ee
Saksham-Arora18 <saksham0666@gmail.com>
2026-07-10 14:41:29 +0530

    TRACKING MODEL AND SCORES WITH MLFLOW

--- network_security/components/model_trainer.py
+++ network_security/components/model_trainer.py
@@ -23,11 +23,8 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier,
 )
-
-
-
-
-
+import mlflow
+from urllib.parse import urlparse
 
 
 class ModelTrainer:
@@ -37,6 +34,25 @@ class ModelTrainer:
             self.data_transformation_artifact=data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+    
+    def track_mlflow(self,best_model,classificationmetric):
+
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+
+            
+
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
+            
+            mlflow.sklearn.log_model(best_model, "model")
+
+
         
 
 
@@ -92,12 +108,16 @@ class ModelTrainer:
         y_train_pred=best_model.predict(X_train)
 
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
+
+        self.track_mlflow(best_model,classification_train_metric)
         
 
 
 
         y_test_pred=best_model.predict(x_test)
         classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
+
+        self.track_mlflow(best_model,classification_test_metric)
 
 
 
